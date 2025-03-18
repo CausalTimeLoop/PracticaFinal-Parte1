1ef3ecff5630d38086b4c2f688856a4efe4328d8
CausalTimeLoop <116341016+CausalTimeLoop@users.noreply.github.com>
2025-03-18 11:57:12 +0100

    Create launch.c
    Testeo y ejemplo de abrir nuevas ventanas de terminar para correr un nuevo programa en ellas

---
+++ launch.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main() {
+  // PERSISTENT TERMINAL
+  // system("gnome-terminal -- bash -c './hello; exec bash'");
+  
+  // TEMPORARY TERMINAL
+  system("gnome-terminal -- bash -c './hello'");
+  
+  return 0;
+}
