90807110b431980f216df89fa4ff095ba2c9b7cb
CausalTimeLoop <116341016+CausalTimeLoop@users.noreply.github.com>
2025-03-18 11:54:20 +0100

    Create cuentasGenerate.c
    primer paso del generador de cuentas predeterminadas en formato binario

---
+++ cuentasGenerate.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Cuenta {
+  int id;            // numero identificador de cuenta
+  char titular [50]; // nombre del titular de cuenta
+  float saldo;       // saldo de cuenta
+  int operaciones;   // numero de operaciones de cuenta
+};
+
+int main() {
+  // declaration default cuentas
+  struct Cuenta cuentas[] = {
+    {1001, "John Doe",       5000.00, 0},
+    {1002, "Freddy Fazbear", 1987.00, 0},
+    {1003, "Ariane Yeong",    512.00, 0}
+  };
+  
+  // open .dat file in binary write mode
+  FILE *file = fopen("cuentas.dat","wb");
+  if (file == NULL) {
+    perror("Error abriendo cuentas.dat");
+    return 1;
+  }
+  
+  // write cuentas to .dat in binary format
+  size_t count = fwrite(cuentas, sizeof(struct Cuenta), 3, file);
+  if (count != 3) {
+    perror("Error writing to file");
+  }
+  
+  // close file
+  fclose(file);
+  printf("Cuentas iniciales generadas y guardadas!\n");
+  
+  return 0;
+}
