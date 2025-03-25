a99a0fb64244ba3fd905cb317414c3fe74fa4e9f
CausalTimeLoop <116341016+CausalTimeLoop@users.noreply.github.com>
2025-03-25 13:35:03 +0100

    hello.c v1.2
    un monton de comentarios para que todos podamos entender y replicar como recibir argumentos

--- hello.c
+++ hello.c
@@ -9,25 +9,34 @@ struct Cuenta {
   float saldo;       // saldo de cuenta
   int operaciones;   // numero de operaciones de cuenta
 };
-
+/*
+argc   == (argument count): number of arguments passed to program
+argv[] == (argument vector): array of strings containing the actual arguments
+argv[0]: current program nameprogram name (hello.c)
+argv[1]: Cuenta id
+argv[2]: Cuenta titular
+argv[3]: Cuenta saldo
+argv[4]: Cuenta operaciones
+*/
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        printf("Uso incorrecto. Ejecutar desde banco.c\n");
-        getchar();
-        return 1;
+    if (argc != 5) { // if all arguments arent correctly passed
+        printf("Uso incorrecto. Ejecutar desde banco.c\n"); // print error message
+        getchar();   // wait for any keypress to be able to see the terminal
+        return 1;    // exit with error code and close terminal
     }
   
 
-  struct Cuenta cuenta;
-  cuenta.id = atoi(argv[1]);
-  strcpy(cuenta.titular, argv[2]); 
-  cuenta.saldo = atof(argv[3]);
-  cuenta.operaciones = atoi(argv[4]);
+  struct Cuenta cuenta; // cuenta struct to store the parsed data
+
+  cuenta.id = atoi(argv[1]);          // convert (id)          from string to integer
+  strcpy(cuenta.titular, argv[2]);    // copy    (titular)     into struct
+  cuenta.saldo = atof(argv[3]);       // convert (saldo)       from string to float
+  cuenta.operaciones = atoi(argv[4]); // convert (operaciones) from string to integer
   
   printf("\n=== Información de la Cuenta ===\n");
-  printf("ID: %d\n", cuenta.id);
-  printf("Titular: %s\n", cuenta.titular);
-  printf("Saldo: %.2f\n", cuenta.saldo);
+  printf("ID: %d\n",          cuenta.id);
+  printf("Titular: %s\n",     cuenta.titular);
+  printf("Saldo: %.2f\n",     cuenta.saldo);
   printf("Operaciones: %d\n", cuenta.operaciones);
   printf("===============================\n");
 
